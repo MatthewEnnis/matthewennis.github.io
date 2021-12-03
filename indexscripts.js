486901311b13daaf209ed40be36df7948a3955f4
MatthewEnnis <mattennis123@gmail.com>
2021-12-03 15:16:47 +0000

    New business card / linktree style

---
+++ indexscripts.js
@@ -0,0 +1,48 @@
+function white() {
+	document.getElementById("m").style.color = "#EEEEEE";
+	document.getElementById("@").style.color = "#EEEEEE";
+	document.getElementById("tthewennis").style.color = "#EEEEEE";
+	document.getElementById("dot").style.color = "#EEEEEE";
+	document.getElementById("info2").style.color = "#EEEEEE";
+}
+
+function email() {
+	document.getElementById("m").style.color = "#E40303";
+	document.getElementById("@").style.color = "#E40303";
+	document.getElementById("tthewennis").style.color = "#E40303";
+	document.getElementById("dot").style.color = "#E40303";
+	document.getElementById("info2").style.color = "#E40303";
+}
+
+function website() {
+	document.getElementById("m").style.color = "#EEEEEE";
+	document.getElementById("@").style.color = "#EEEEEE";
+	document.getElementById("tthewennis").style.color = "#FF8C00";
+	document.getElementById("dot").style.color = "#FF8C00";
+	document.getElementById("info2").style.color = "#FF8C00";
+}
+
+function twitter() {
+	document.getElementById("m").style.color = "#EEEEEE";
+	document.getElementById("@").style.color = "#FFED00";
+	document.getElementById("tthewennis").style.color = "#FFED00";
+	document.getElementById("dot").style.color = "#EEEEEE";
+	document.getElementById("info2").style.color = "#EEEEEE";
+}
+
+function insta() {
+	document.getElementById("m").style.color = "#EEEEEE";
+	document.getElementById("@").style.color = "#008026";
+	document.getElementById("tthewennis").style.color = "#008026";
+	document.getElementById("dot").style.color = "#EEEEEE";
+	document.getElementById("info2").style.color = "#EEEEEE";
+}
+
+function info() {
+	document.getElementById("m").style.color = "#EEEEEE";
+	document.getElementById("@").style.color = "#EEEEEE";
+	document.getElementById("tthewennis").style.color = "#EEEEEE";
+	document.getElementById("dot").style.color = "#EEEEEE";
+	document.getElementById("info2").style.color = "#004DFF";
+}
+
