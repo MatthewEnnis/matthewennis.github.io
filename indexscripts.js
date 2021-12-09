ebe464c95143983e8e4b802a6dbe591e6156c63a
MatthewEnnis <mattennis123@gmail.com>
2021-12-09 13:27:33 +0000

    Fancy new look

--- indexscripts.js
+++ indexscripts.js
@@ -1,48 +1,62 @@
-function white() {
-	document.getElementById("m").style.color = "#EEEEEE";
-	document.getElementById("@").style.color = "#EEEEEE";
-	document.getElementById("tthewennis").style.color = "#EEEEEE";
-	document.getElementById("dot").style.color = "#EEEEEE";
-	document.getElementById("info2").style.color = "#EEEEEE";
+function grey() {
+	document.getElementById("m").style.color = "#121212";
+	document.getElementById("@").style.color = "#121212";
+	document.getElementById("tthew").style.color = "#121212";
+	document.getElementById("ennis").style.color = "#121212";
+	document.getElementById("dot").style.color = "#121212";
+	document.getElementById("info2").style.color = "#121212";
+}
+
+function youtube() {
+	document.getElementById("m").style.color = "#E40303";
+	document.getElementById("@").style.color = "#E40303";
+	document.getElementById("tthew").style.color = "#E40303";
+	document.getElementById("ennis").style.color = "#121212";
+	document.getElementById("dot").style.color = "#121212";
+	document.getElementById("info2").style.color = "#121212";
 }
 
 function email() {
-	document.getElementById("m").style.color = "#E40303";
-	document.getElementById("@").style.color = "#E40303";
-	document.getElementById("tthewennis").style.color = "#E40303";
-	document.getElementById("dot").style.color = "#E40303";
-	document.getElementById("info2").style.color = "#E40303";
-}
-
-function website() {
-	document.getElementById("m").style.color = "#EEEEEE";
-	document.getElementById("@").style.color = "#EEEEEE";
-	document.getElementById("tthewennis").style.color = "#FF8C00";
+	document.getElementById("m").style.color = "#FF8C00";
+	document.getElementById("@").style.color = "#FF8C00";
+	document.getElementById("tthew").style.color = "#FF8C00";
+	document.getElementById("ennis").style.color = "#FF8C00";
 	document.getElementById("dot").style.color = "#FF8C00";
 	document.getElementById("info2").style.color = "#FF8C00";
 }
 
-function twitter() {
-	document.getElementById("m").style.color = "#EEEEEE";
-	document.getElementById("@").style.color = "#FFED00";
-	document.getElementById("tthewennis").style.color = "#FFED00";
-	document.getElementById("dot").style.color = "#EEEEEE";
-	document.getElementById("info2").style.color = "#EEEEEE";
+function website() {
+	document.getElementById("m").style.color = "#121212";
+	document.getElementById("@").style.color = "#121212";
+	document.getElementById("tthew").style.color = "#FFED00";
+	document.getElementById("ennis").style.color = "#FFED00";
+	document.getElementById("dot").style.color = "#FFED00";
+	document.getElementById("info2").style.color = "#FFED00";
 }
 
 function insta() {
-	document.getElementById("m").style.color = "#EEEEEE";
+	document.getElementById("m").style.color = "#121212";
 	document.getElementById("@").style.color = "#008026";
-	document.getElementById("tthewennis").style.color = "#008026";
-	document.getElementById("dot").style.color = "#EEEEEE";
-	document.getElementById("info2").style.color = "#EEEEEE";
+	document.getElementById("tthew").style.color = "#008026";
+	document.getElementById("ennis").style.color = "#008026";
+	document.getElementById("dot").style.color = "#121212";
+	document.getElementById("info2").style.color = "#121212";
+}
+
+function twitter() {
+	document.getElementById("m").style.color = "#121212";
+	document.getElementById("@").style.color = "#004DFF";
+	document.getElementById("tthew").style.color = "#004DFF";
+	document.getElementById("ennis").style.color = "#004DFF";
+	document.getElementById("dot").style.color = "#121212";
+	document.getElementById("info2").style.color = "#121212";
 }
 
 function info() {
-	document.getElementById("m").style.color = "#EEEEEE";
-	document.getElementById("@").style.color = "#EEEEEE";
-	document.getElementById("tthewennis").style.color = "#EEEEEE";
-	document.getElementById("dot").style.color = "#EEEEEE";
-	document.getElementById("info2").style.color = "#004DFF";
+	document.getElementById("m").style.color = "#121212";
+	document.getElementById("@").style.color = "#121212";
+	document.getElementById("tthew").style.color = "#121212";
+	document.getElementById("ennis").style.color = "#121212";
+	document.getElementById("dot").style.color = "#121212";
+	document.getElementById("info2").style.color = "#750787";
 }
-
